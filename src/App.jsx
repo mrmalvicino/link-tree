ce12271fe56ca2aae6f3d14c8e06777982cccb95
mrmalvicino <mrmalvicino@gmail.com>
2025-03-31 21:21:11 -0300

    :sparkles: Feat: Added email to the list of links with optional parameters
    - Display a check icon when copying a link without a URL
    - Make `textSize` an optional prop
    - Prevent `<i>` tag from rendering if `icon` is not provided

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ export function App() {
     return (
         <section className="fluo-box col-flex">
             <h1>M.R.Malvicino</h1>
-            <div className="col-flex">
+            <div className="col-flex mb-2">
                 <LinkEntry text="Portfolio" url="https://mrmalvicino.com" icon="briefcase" />
                 <LinkEntry text="LinkedIn" url="https://www.linkedin.com/in/mrmalvicino/" icon="linkedin" />
                 <LinkEntry text="Telegram" url="https://t.me/mrmalvicino" icon="telegram" />
             </div>
+            <LinkEntry text="mrmalvicino@gmail.com" textSize="1.4rem" />
         </section>
     )
 }
--- src/LinkEntry.jsx
+++ src/LinkEntry.jsx
@@ -1,17 +1,36 @@
-export function LinkEntry({ text, url, icon }) {
+import React, { useState } from "react";
+
+export function LinkEntry({ text, url, icon, textSize = "1.6rem" }) {
+    const [copied, setCopied] = useState(false);
+
     const props = {
         style: {
-            fontSize: "1.6rem",
+            fontSize: textSize,
             color: "#fafafa",
             textDecoration: "none"
         }
     }
 
+    const handleClick = (event) => {
+        if (!url) {
+            event.preventDefault();
+
+            navigator.clipboard.writeText(text)
+                .then(() => {
+                    setCopied(true);
+                    setTimeout(() => setCopied(false), 2000);
+                })
+                .catch(err => {
+                    console.error("Error: ", err);
+                });
+        }
+    };
+
     return (
-        <a href={url} {...props}>
-            <i className={`bi bi-${icon}`} style={{ verticalAlign: "middle" }}></i>
-            <span>{` `}</span>
+        <a href={url || "#"} onClick={(e) => handleClick(e, text, url)} {...props} target="_blank" rel="noopener noreferrer">
+            {icon && <i className={`bi bi-${icon}`} style={{ verticalAlign: "middle", marginRight: "10px" }}></i>}
             <span>{text}</span>
+            {copied && <i className="bi bi-check" style={{ verticalAlign: "middle", color: "#0f0" }}></i>}
         </a>
     )
 }
